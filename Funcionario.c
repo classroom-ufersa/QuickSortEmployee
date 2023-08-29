2d57dd8bb8062af29bcb225c4f200bc7c9ee5145
Pedro Raulino <116983278+pedrohraulino@users.noreply.github.com>
2023-08-29 16:39:21 -0300

    Update Funcionario.c

--- Funcionario.c
+++ Funcionario.c
@@ -29,23 +29,3 @@ void imprimir(Funcionario **funcionario, int n){
       printf("Documento: %i\n", funcionario[i]->documento);   
     }
 }
-int main(void){
-  int n;
-      printf("Digite a quantidade de funcionarios: \n");
-      scanf("%i", &n);
-  Funcionario **funcionario = (Funcionario**) malloc(n * sizeof(Funcionario*));
-     if(funcionario == NULL){
-        printf("Memoria insuficiente!");
-        exit(1);
-     }
-     for(int i = 0; i < n; i++){
-        funcionario[i] = (Funcionario*) malloc(n * sizeof(Funcionario));
-     }
-     preencher(funcionario, n);
-     imprimir(funcionario, n);
-     for(int i = 0; i < n; i++){
-        free(funcionario[i]);
-     }
-     free(funcionario);
-     return 0;
-}
