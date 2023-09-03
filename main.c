016e93cc1bc22b8cd59ba7094eca051666b1ce7c
TheFonci <115956936+TheFonci@users.noreply.github.com>
2023-09-03 16:22:33 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,23 +2,62 @@
 #include<stdlib.h>
 #include<string.h>
 
-int main(void){
-  int n;
-      printf("Digite a quantidade de funcionarios: \n");
-      scanf("%i", &n);
-  Funcionario **funcionario = (Funcionario**) malloc(n * sizeof(Funcionario*));
-     if(funcionario == NULL){
-        printf("Memoria insuficiente!");
-        exit(1);
-     }
-     for(int i = 0; i < n; i++){
-        funcionario[i] = (Funcionario*) malloc(n * sizeof(Funcionario));
-     }
-     preencher(funcionario, n);
-     imprimir(funcionario, n);
-     for(int i = 0; i < n; i++){
-        free(funcionario[i]);
-     }
-     free(funcionario);
-     return 0;
+int main(void)
+{
+    Funcionario *funcionario = NULL;
+    int n = 0, opcao;
+
+    do
+    {
+        printf("\nMenu:\n");
+        printf("1 - Cadastrar funcionario\n");
+        printf("2 - Ver funcionarios\n");
+        printf("3 - Encerrar programa\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+        case 1:
+            while (1)
+            {
+
+                if (opcao == 2)
+                {
+                    break;
+                }
+
+                funcionario = (Funcionario *)realloc(funcionario, (n + 1) * sizeof(Funcionario));
+
+                if (funcionario == NULL)
+                {
+                    printf("Memoria insuficiente!\n");
+                    exit(1);
+                }
+
+                preencher(&funcionario[n]); // Passa o endereço do funcionário atual
+                n++;
+                printf("Registrar novo funcionario?\n1 = Sim\t2 = Nao: ");
+                scanf("%d", &opcao);
+            }
+        break;
+
+        case 2:
+            imprimir(funcionario, n);
+            break;
+
+        case 3:
+            printf("Encerrando o programa...\n");
+            break;
+
+        default:
+            printf("\nOpcao invalida. Tente novamente.\n");
+            break;
+        }
+
+    } while (opcao != 3);
+
+    free(funcionario);
+
+    return 0;
 }
