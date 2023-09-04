9cdf9552fc57b55847e061dff61b60093bfdcf20
Pedro Raulino <116983278+pedrohraulino@users.noreply.github.com>
2023-09-04 09:58:49 -0300

    Update Funcionario.c
    adição da função para ordenar as linhas do arquivo

--- Funcionario.c
+++ Funcionario.c
@@ -18,12 +18,11 @@ void preencher(Funcionario *funcionario)
     scanf("%d", &funcionario->documento);
 }
 
-int compararFuncionarios(const void *a, const void *b)
-{
-    const Funcionario *funcionarioA = (const Funcionario *)a;
-    const Funcionario *funcionarioB = (const Funcionario *)b;
-    return strcmp(funcionarioA->nome, funcionarioB->nome);
+
+int compararLinhas(const void *a, const void *b) {
+    return strcmp(*(const char **)a, *(const char **)b);
 }
+
 
 void imprimirArquivo()
 {
@@ -46,36 +45,96 @@ void imprimirArquivo()
 
 void criarArquivo(Funcionario *funcionario, int n)
 {
-    // Abra o arquivo em modo anexar para adicionar os dados
+    // Abre o arquivo em modo leitura para verificar o cabeçalho
+    FILE *arquivoVerificacao = fopen("funcionarios.txt", "r");
+
+    // Verifica se o cabeçalho já existe
+    int cabecalhoExiste = 0;
+    char linha[100];
+    while (fgets(linha, sizeof(linha), arquivoVerificacao) != NULL)
+    {
+        if (strstr(linha, "*Funcionários cadastrados*") != NULL)
+        {
+            cabecalhoExiste = 1;
+            break;
+        }
+    }
+
+    fclose(arquivoVerificacao);
+
+    // Abre o arquivo em modo anexar para adicionar os dados
     FILE *arquivo = fopen("funcionarios.txt", "a");
 
     if (arquivo == NULL)
     {
-        printf("Erro ao abrir o arquivo.\n");
+        printf("Erro ao criar o arquivo.\n");
         return;
     }
 
-    // Verifique se o arquivo está vazio
-    fseek(arquivo, 0, SEEK_END);
-    int arquivoVazio = ftell(arquivo) == 0;
-    rewind(arquivo);
-
-    if (arquivoVazio)
+    if (!cabecalhoExiste)
     {
-        fprintf(arquivo, "*Funcionários cadastrados (ordenados por nome)*\n");
+        fprintf(arquivo, "*Funcionários cadastrados*\n");
         fprintf(arquivo, "Nome\t Cargo\t Documento\t\n");
     }
-
-    // Ordenar os funcionários antes de escrevê-los no arquivo
-    qsort(funcionario, n, sizeof(Funcionario), compararFuncionarios);
-
     for (int i = 0; i < n; i++)
     {
         fprintf(arquivo, "%s\t", funcionario[i].nome);
         fprintf(arquivo, "%s\t", funcionario[i].cargo);
         fprintf(arquivo, "%d\t\n", funcionario[i].documento);
     }
+    
+    fclose(arquivo);
+
+    printf("Dados salvos em 'funcionarios.txt'\n");
+}
+
+
+void ordenarLinhasArquivo() {
+    FILE *arquivo = fopen("funcionarios.txt", "r");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo");
+        exit(1);
+    }
+
+    char **linhas = NULL;
+    char linha[100];
+    int numLinhas = 0;
+    int linhaAtual = 0; // Variável de controle para ignorar as duas primeiras linhas
+
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+        if (linhaAtual >= 2) { // Ignora as duas primeiras linhas
+            // Remove o caractere de nova linha
+            linha[strcspn(linha, "\n")] = '\0';
+            
+            // Aloca memória para a linha e copia o conteúdo
+            linhas = realloc(linhas, (numLinhas + 1) * sizeof(char *));
+            linhas[numLinhas] = strdup(linha);
+            numLinhas++;
+        }
+        linhaAtual++;
+    }
 
     fclose(arquivo);
-    printf("Dados salvos em 'funcionarios.txt'\n");
+
+    // Ordena as linhas usando qsort
+    qsort(linhas, numLinhas, sizeof(char *), compararLinhas);
+
+    // Reabre o arquivo em modo de escrita (sobrescrevendo o conteúdo existente)
+    arquivo = fopen("funcionarios.txt", "w");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo para escrita");
+        exit(1);
+    }
+    
+    fprintf(arquivo, "*Funcionários cadastrados*\n");
+    fprintf(arquivo, "Nome\t Cargo\t Documento\t\n");
+
+    // Escreve as linhas ordenadas de volta para o arquivo
+    for (int i = 0; i < numLinhas; i++) {
+        fprintf(arquivo, "%s\n", linhas[i]);
+        free(linhas[i]); // Libera a memória alocada para a linha
+    }
+
+    fclose(arquivo);
+    free(linhas);
 }
