9548367e3ae4e183e77b4a2a59d13e310c085ff9
TheFonci <115956936+TheFonci@users.noreply.github.com>
2023-09-04 23:26:47 -0300

    Update main.c

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ int main(void)
 {
     Funcionario *funcionario = NULL;
     int n = 0, opcao;
+    int valorValido=0;
     do
     {
         printf("\nMenu:\n");
@@ -15,32 +16,63 @@ int main(void)
         printf("2 - Ver funcionarios\n");
         printf("3 - Encerrar programa\n");
         printf("Escolha uma opcao: ");
-        scanf("%d", &opcao);
+        while (!valorValido) {
+           if (scanf("%d", &opcao) == 1) {
+                // A leitura foi bem-sucedida, o valor é um número inteiro
+                if (opcao <= 3) {
+                    // Verifica se o número é menor ou igual a 3
+                    valorValido = 1;
+                } else {
+                    printf("Digite uma das opcoes do menu.\n");
+                }
+            } else {
+                // A leitura não foi bem-sucedida, o valor não é um número inteiro
+                printf("Digite uma das opcoes do menu.\n");
+                while (getchar() != '\n');
+            }
+        }
+        valorValido=0;
 
         switch (opcao)
         {
         case 1:
-            while (opcao != 2)
-            {
-                funcionario = (Funcionario *)realloc(funcionario, (n+1) * sizeof(Funcionario));
+            
+            while (opcao != 2){
+            funcionario = (Funcionario *)realloc(funcionario, (n+1) * sizeof(Funcionario));
 
-                if (funcionario == NULL)
-                {
-                    printf("Memoria insuficiente!\n");
+            if (funcionario == NULL) {
+                    printf("Memória insuficiente!\n");
                     exit(1);
+            }
+
+            preencher(&funcionario[n]); // Passa o endereço do funcionário atual
+            n++;
+
+            printf("Registrar novo funcionário?\n1 = Sim, 2 = Não: ");
+            while (!valorValido) {
+                if (scanf("%d", &opcao) == 1) {
+                    // A leitura foi bem-sucedida, o valor é um número inteiro
+                    if (opcao == 1) {
+                        valorValido = 1; // Se o usuário escolher 1, o loop continua
+                    } else if (opcao == 2) {
+                        valorValido = 1; // Se o usuário escolher 2, o loop termina
+                    } else {
+                        printf("Digite uma das opções do menu (1 ou 2).\n");
+                    }
+                } else {
+                    // A leitura não foi bem-sucedida, o valor não é um número inteiro
+                    printf("Digite uma das opções do menu (1 ou 2).\n");
+                    while (getchar() != '\n');
                 }
-
-                preencher(&funcionario[n]); // Passa o endereço do funcionário atual
-                n++;
-
-                printf("Registrar novo funcionario?\n1 = Sim\t2 = Nao: ");
-                scanf("%d", &opcao);
+            }
+            valorValido = 0;
             }
             criarArquivo(funcionario,n);
             free(funcionario);
             funcionario = NULL;
             n=0;
             ordenarLinhasArquivo();
+
         break;
         case 2:
             ordenarLinhasArquivo();
